e1174e00ba0770b811f867791c6ae99d4d5bf1bc
DasanKim <tempzipp@gmail.com>
2024-01-17 09:46:34 +0900

    ✅test: ImageManagerTests 생성

---
+++ BoxOfficeTests/ImageManagerTests/ImageManagerTests.swift
@@ -0,0 +1,54 @@
+//
+//  ImageManagerTests.swift
+//  DecodingManagerTests
+//
+//  Created by Dasan on 2024/01/10.
+//
+
+import XCTest
+@testable import BoxOffice
+
+final class ImageManagerTests: XCTestCase {
+    private let image = UIImage(named: "movie_sample_image")
+    private var imageManager: ImageManager!
+    
+    func testFetchImage() {
+        let expectation = XCTestExpectation(description: "fetchImage")
+        expectation.expectedFulfillmentCount = 1
+        
+        let data: Data = image!.jpegData(compressionQuality: 1)!
+        let networkManager = MockSuccessNetworkManager(data: data)
+        imageManager = ImageManager(networkManager: networkManager)
+        
+        imageManager.fetchImage(url: URL(string: "sample1")) { result in
+            switch result {
+            case .success(let image):
+                networkManager.verify(url: URL(string: "sample1"),
+                                      method: .get,
+                                      header: nil)
+                XCTAssertNotNil(image)
+                expectation.fulfill()
+            case .failure:
+                XCTFail()
+            }
+        }
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testFetchImageFailure() {
+        let expectation = XCTestExpectation(description: "fetchImageFailure")
+        
+        let networkManager = MockFailureNetworkManager(error: .noData)
+        imageManager = ImageManager(networkManager: networkManager)
+        
+        imageManager.fetchImage(url: URL(string: "sample1")) { result in
+            switch result {
+            case .success:
+                XCTFail()
+            case .failure:
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation], timeout: 5.0)
+    }
+}
