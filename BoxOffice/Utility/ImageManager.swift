68bc411e436a206c07e9b7c4472a5995c979798f
DasanKim <tempzipp@gmail.com>
2024-01-17 09:25:37 +0900

    ♻️refactor: NetworkManager 수정 및 ImageManager 생성

---
+++ BoxOffice/Utility/ImageManager.swift
@@ -0,0 +1,30 @@
+//
+//  ImageManager.swift
+//  BoxOffice
+//
+//  Created by Dasan on 2024/01/09.
+//
+
+import UIKit.UIImage
+
+final class ImageManager {
+    private let networkManager: NetworkManageable
+    
+    init(networkManager: NetworkManageable = NetworkManager(requester: DefaultRequester())) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchImage(url: URL?, completion: @escaping (Result<UIImage?, NetworkError>) -> Void) {
+        let _ = networkManager.fetchData(from: url,
+                                         method: .get,
+                                         header: nil) { result in
+            switch result {
+            case .success(let data):
+                guard let image = UIImage(data: data) else { return }
+                completion(.success(image))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
--- BoxOffice/Utility/NetworkManager.swift
+++ BoxOffice/Utility/NetworkManager.swift
@@ -7,7 +7,68 @@
 
 import UIKit
 
-struct NetworkManager {
+final class NetworkManager: NetworkManageable {
+    var requester: Requestable
+    
+    init(requester: Requestable = DefaultRequester()) {
+        self.requester = requester
+    }
+    
+    @discardableResult
+    func fetchData(from url: URL?, method: HTTPMethod, header: [HTTPHeader]?, completion: @escaping (Result<Data, NetworkError>) -> Void) -> URLSessionDataTask? {
+        guard let urlRequest = makeURLRequest(url: url, method: method, headers: header) else {
+            completion(.failure(.invalidURL))
+            return nil
+        }
+        
+        let dataTask = requester.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil else {
+                completion(.failure(NetworkError.requestFailed))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NetworkError.invalidResponse))
+                
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
+                
+                return
+            }
+            
+            completion(.success(data))
+        }
+        
+        dataTask.resume()
+        
+        return dataTask
+    }
+    
+    private func makeURLRequest(url: URL?, method: HTTPMethod, headers: [HTTPHeader]?) -> URLRequest? {
+        guard let url = url else { return nil }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method.rawValue
+        headers?.forEach {
+            $0.header.forEach { field, value in
+                urlRequest.addValue(value, forHTTPHeaderField: field)
+            }
+        }
+        return urlRequest
+    }
+}
+
+final class DefaultRequester: Requestable {
+    func dataTask(with urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return URLSession.shared.dataTask(with: urlRequest, completionHandler: completionHandler)
+    }
+}
+
+struct NetworkManager2 {
     func fetchData(url: URL?, completion: @escaping (Result<Data?, NetworkError>) -> Void) {
         guard let url = url else { return }
         
@@ -35,6 +96,7 @@ struct NetworkManager {
         }
         
         task.resume()
+        // Task가 완료 후 completion handler가 실행된다.
     }
     
     func fetchData(urlRequest: URLRequest?, completion: @escaping (Result<Data?, NetworkError>) -> Void) {
