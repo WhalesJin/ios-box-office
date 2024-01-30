e44232f0d7b2c71f11619658a2e9f09fd2b916af
DasanKim <tempzipp@gmail.com>
2024-01-30 15:01:35 +0900

    ✅test: BoxOfficeManagerTests 수정

--- BoxOfficeTests/BoxOfficeManagerTests/BoxOfficeManagerTests.swift
+++ BoxOfficeTests/BoxOfficeManagerTests/BoxOfficeManagerTests.swift
@@ -14,27 +14,32 @@ final class BoxOfficeManagerTests: XCTestCase {
     private var boxOfficeManager: BoxOfficeManager!
     
     func testFetchBoxOfficeData() {
+        let expectation = XCTestExpectation(description: "fetchBoxOffice")
         let dataAsset: NSDataAsset = NSDataAsset(name: "box_office_sample")!
 
         let networkManager = NetworkManager(requester: SuccessRequester(data: dataAsset.data))
         //let networkManager = MockSuccessNetworkManager(data: dataAsset.data)
         boxOfficeManager = BoxOfficeManager(networkManager: networkManager)
 
-        let expectation = "경관의 피"
+        let expectationMovieName = "경관의 피"
         
         boxOfficeManager.fetchBoxOfficeData(with: TargetDate(dayFromNow: 0)) { result in
             switch result {
             case .success(let data):
-                XCTAssertEqual(data!.first?.movieName, expectation)
+                XCTAssertEqual(data!.first?.movieName, expectationMovieName)
+                expectation.fulfill()
             case .failure:
                 XCTFail()
             }
         }
+        wait(for: [expectation], timeout: 0.5)
     }
     
     func testFetchBoxOfficeDataFailure() {
+        let expectation = XCTestExpectation(description: "fetchBoxOfficeFailure")
         let dataAsset: NSDataAsset = NSDataAsset(name: "movie_Info_sample")! //다른 타입의 JSON
         let networkManager = NetworkManager(requester: SuccessRequester(data: dataAsset.data))
+        //let networkManager = MockSuccessNetworkManager(data: dataAsset.data)
         boxOfficeManager = BoxOfficeManager(networkManager: networkManager)
 
         boxOfficeManager.fetchBoxOfficeData(with: TargetDate(dayFromNow: 0)) { result in
@@ -43,30 +48,38 @@ final class BoxOfficeManagerTests: XCTestCase {
                 XCTFail()
             case .failure(let error):
                 XCTAssertEqual(error as! DataError, DataError.failedDecoding)
+                expectation.fulfill()
             }
         }
+        wait(for: [expectation], timeout: 0.5)
     }
     
     func testFetchMovieData() {
+        let expectation = XCTestExpectation(description: "fetchMovieData")
         let dataAsset: NSDataAsset = NSDataAsset(name: "movie_Info_sample")!
         let networkManager = NetworkManager(requester: SuccessRequester(data: dataAsset.data))
+        //let networkManager = MockSuccessNetworkManager(data: dataAsset.data)
         boxOfficeManager = BoxOfficeManager(networkManager: networkManager)
 
-        let expectation = "이병헌"
+        let expectationPersonName = "이병헌"
 
         boxOfficeManager.fetchMovieData(with: "123456"){ result in
             switch result {
             case .success(let data):
-                XCTAssertEqual(data!.actors.first?.personName, expectation)
+                XCTAssertEqual(data!.actors.first?.personName, expectationPersonName)
+                expectation.fulfill()
             case .failure:
                 XCTFail()
             }
         }
+        wait(for: [expectation], timeout: 0.5)
     }
     
     func testFetchMovieDataFailure() {
+        let expectation = XCTestExpectation(description: "fetchMovieDataFailure")
         let dataAsset: NSDataAsset = NSDataAsset(name: "box_office_sample")!
         let networkManager = NetworkManager(requester: SuccessRequester(data: dataAsset.data))
+        //let networkManager = MockSuccessNetworkManager(data: dataAsset.data)
         boxOfficeManager = BoxOfficeManager(networkManager: networkManager)
 
         boxOfficeManager.fetchMovieData(with: "movieCode") { result in
@@ -75,30 +88,38 @@ final class BoxOfficeManagerTests: XCTestCase {
                 XCTFail()
             case .failure(let error):
                 XCTAssertEqual(error as! DataError, DataError.failedDecoding)
+                expectation.fulfill()
             }
         }
+        wait(for: [expectation], timeout: 0.5)
     }
     
     func testFetchMovieImageData() {
+        let expectation = XCTestExpectation(description: "fetchMovieImageData")
         let dataAsset: NSDataAsset = NSDataAsset(name: "movie_image_sample")!
         let networkManager = NetworkManager(requester: SuccessRequester(data: dataAsset.data))
+        //let networkManager = MockSuccessNetworkManager(data: dataAsset.data)
         boxOfficeManager = BoxOfficeManager(networkManager: networkManager)
 
-        let expectation = URL(string: "https://www.imageUrl.com")
+        let expectationImageURL = URL(string: "https://www.imageUrl.com")
 
         boxOfficeManager.fetchMovieImageData(with: "keyword") { result in
             switch result {
             case .success(let data):
-                XCTAssertEqual(data!, expectation)
+                XCTAssertEqual(data!, expectationImageURL)
+                expectation.fulfill()
             case .failure:
                 XCTFail()
             }
         }
+        wait(for: [expectation], timeout: 0.5)
     }
     
     func testFetchMovieImageDataFailure() {
+        let expectation = XCTestExpectation(description: "fetchMovieImageDataFailure")
         let dataAsset: NSDataAsset = NSDataAsset(name: "box_office_sample")!
         let networkManager = NetworkManager(requester: SuccessRequester(data: dataAsset.data))
+        //let networkManager = MockSuccessNetworkManager(data: dataAsset.data)
         boxOfficeManager = BoxOfficeManager(networkManager: networkManager)
         
         boxOfficeManager.fetchMovieImageData(with: "keyword") { result in
@@ -107,7 +128,9 @@ final class BoxOfficeManagerTests: XCTestCase {
                 XCTFail()
             case .failure(let error):
                 XCTAssertEqual(error as! DataError, DataError.failedDecoding)
+                expectation.fulfill()
             }
         }
+        wait(for: [expectation], timeout: 0.5)
     }
 }
